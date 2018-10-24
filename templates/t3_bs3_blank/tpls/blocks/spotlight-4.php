9799208cb11e2bc61c6f9ebb96641298b23dcf49
elfpower <elfpower@qq.com>
2018-10-24 16:28:54 +0800

    init-kudos
    init-kudos 迁移初始化(前端与后台系统汉化、T3安装与汉化、T3布局与模块修改、akeeba安装)

---
+++ templates/t3_bs3_blank/tpls/blocks/spotlight-4.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package   T3 Blank
+ * @copyright Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<?php if ($this->checkSpotlight('spotlight-4', 'position-19, position-20, position-21, position-22, position-23, position-24')) : ?>
+	<!-- SPOTLIGHT 4 -->
+	<div class="container t3-sl t3-sl-4">
+		<?php $this->spotlight('spotlight-4', 'position-19, position-20, position-21, position-22, position-23, position-24') ?>
+	</div>
+	<!-- //SPOTLIGHT 4 -->
+<?php endif ?>
