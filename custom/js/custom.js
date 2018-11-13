2bedaf51992cd2bce37d48af54bb31c30070cc81
titan_oO <elfpower@qq.com>
2018-11-13 17:41:09 +0800

    edit-naws-side
    新闻侧栏布局

---
+++ custom/js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+  
+  $('.t3-spotlight > div').each(function (v,k) {
+    var o = $('.t3-spotlight > div').eq(v)
+    if(o.children().length < 1) {
+      o.css("display","none") 
+    }
+  })
+  
+})
